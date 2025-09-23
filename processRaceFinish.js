dd96237527adc0157d6135cff60c4620d6aeeb1e
SitcomReality <SitcomReality@websim.ai>
2025-09-23 05:01:02 +0000

    v105: error: Uncaught ReferenceError: setStep is not defined ReferenceError: setStep is not defined     at processRaceFinish      at race

--- processRaceFinish.js
+++ processRaceFinish.js
@@ -5,11 +5,12 @@ function processRaceFinish() {
 		document.getElementById('startRaceWeek').disabled = false;
 		document.getElementById('setupRace').disabled = true;
 		console.log("DEBUG: the week was detected ended from beginRace()");
-		setStep(4,'done'); setStep(2,'active'); setStatus('Week complete! Start a new Race Week when ready.');
+		HUD.setStep(4,'done'); HUD.setStep(2,'active'); HUD.setStatus('Week complete! Start a new Race Week when ready.');
 	} else {
 		document.getElementById('setupRace').disabled = false;
 		document.getElementById('startRace').disabled = true;
-		setStep(4,'done'); setStep(3,'active'); setStatus('Race finished. Setup the next race.');
+		HUD.setStep(4,'done'); HUD.setStep(3,'active'); HUD.setStatus('Race finished. Setup the next race.');
 	}
 	if (window.canvasRenderer) window.canvasRenderer.stop();
 }
+
