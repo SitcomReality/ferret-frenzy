a989b20dd93705ebad54cd82eaef208d386348fd
SitcomReality <SitcomReality@websim.ai>
2025-09-26 16:00:30 +0000

    v512: Uncaught SyntaxError: Invalid or unexpected token at /render/renderers/FerretLegRenderer.js:183:45

--- render/renderers/FerretLegRenderer.js
+++ render/renderers/FerretLegRenderer.js
@@ -180,10 +180,14 @@ export class FerretLegRenderer {
     G = (G<255)?G:255;  
     B = (B<255)?B:255;  
 
-    const RR = ((R.toString(16).length===1)?\"0\"+R.toString(16):R.toString(16));
-    const GG = ((G.toString(16).length===1)?\"0\"+G.toString(16):G.toString(16));
-    const BB = ((B.toString(16).length===1)?\"0\"+B.toString(16):B.toString(16));
+    R = Math.round(R);
+    G = Math.round(G);
+    B = Math.round(B);
 
-    return \"#\"+RR+GG +BB;
+    const RR = ((R.toString(16).length===1)?"0"+R.toString(16):R.toString(16));
+    const GG = ((G.toString(16).length===1)?"0"+G.toString(16):G.toString(16));
+    const BB = ((B.toString(16).length===1)?"0"+B.toString(16):B.toString(16));
+
+    return "#"+RR+GG+BB;
   }
 }
